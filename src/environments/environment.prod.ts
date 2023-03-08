e6ac02f6870c8199ced9b9ab9147faf5b9882ebe
Rafael Silva de Lima <rafaelsilvadelimaoficial@gmail.com>
2023-03-08 12:38:00 -0300

    Ajustes de responsividade para o modelo galaxy fold

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,11 @@
 export const environment = {
     firebase: {
-
+      apiKey: "AIzaSyDoxlc0onSU0f5uMlvdiQdkMepyZI1pKC8",
+      authDomain: "nerd-nao-reclama.firebaseapp.com",
+      projectId: "nerd-nao-reclama",
+      storageBucket: "nerd-nao-reclama.appspot.com",
+      messagingSenderId: "502964505239",
+      appId: "1:502964505239:web:556d2393a23b07101eb7d2"
     },
     production: true
   };
