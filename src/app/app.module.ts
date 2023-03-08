cf9668288f4b9959fe43811bf6db099ec3b0f388
Rafael Silva de Lima <rafaelsilvadelimaoficial@gmail.com>
2023-03-07 21:22:11 -0300

    first commit - Desenvolvido tela home, trabalhado um pouco da responsividade mobile, inserido alguns links, footer. Projeto em seus primeiros passos.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideStorage,getStorage } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFireModule } from '@angular/fire/compat';
+import { firebaseConfig } from 'src/config/firebase.config';
+import { ToastrModule } from 'ngx-toastr';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
+    AngularFireAuthModule,
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    ToastrModule.forRoot({
+      timeOut: 4000,
+      progressBar: true,
+      closeButton: true,
+      positionClass :'toast-top-center'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
